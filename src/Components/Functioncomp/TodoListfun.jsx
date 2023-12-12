5cc10c02a3762f87e8cf6cd49a90a4751bea212e
Ksangavikarthi <ksangavi2002@gmail.com>
2023-12-12 18:08:42 +0530

    tag add

--- src/Components/Functioncomp/TodoListfun.jsx
+++ src/Components/Functioncomp/TodoListfun.jsx
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 import './Todolist.css'
 
 export default function TodoListfun() {
-    let uuidver=uuidv4()
+    let uuidver = uuidv4()
     const [data, setdata] = useState('')
 
     //default list 
@@ -14,41 +14,41 @@ export default function TodoListfun() {
         },
         {
             id: 2,
-            task: 'reading'
+            task: 'Reading'
         }
     ])
 
     const handle = (e) => {
         setdata(e.target.value)
     }
-    const handlechange=()=>{
-        settodolist([...todolist,{id:uuidver,task:data}])
+    const handlechange = () => {
+        settodolist([...todolist, { id: uuidver, task: data }])
     }
 
-    const handledelete=(deleteid)=>{
-        let filtered=todolist.filter((del)=>del.id !=deleteid)
+    const handledelete = (deleteid) => {
+        let filtered = todolist.filter((del) => del.id != deleteid)
         settodolist(filtered)
     }
-    const handleupdate=(i)=>{
-        let update=prompt('update ur task',todolist[i].task)
-        let copy=[...todolist]
-        copy[i].task=update
+    const handleupdate = (i) => {
+        let update = prompt('update ur task', todolist[i].task)
+        let copy = [...todolist]
+        copy[i].task = update
         settodolist(copy)
     }
     return (
         <div id="project">
-            <h1>DAILY WORK </h1>
+            <h1 id='head'>DAILY WORK </h1>
             <div className="inputbox">
-            <input type="text" placeholder="tasks" onChange={(e) => handle(e)} />
-            {/* <p>{data}</p>    //input box what letter type it printed  */}
-            <button className="btn" onClick={handlechange}>Addd task</button>
+                <input className='input' type="text" placeholder="Tasks" onChange={(e) => handle(e)} />
+                <button className="bt" onClick={handlechange}>Add task</button>
             </div>
-            <br /><br />
-            {todolist.map((list,i) => (
+            <br />
+            {todolist.map((list, i) => (
                 <div key={i} className="lists">
-                    <span>{list.task}</span>
-                    <button onClick={()=>handleupdate(i)}>update</button>
-                    <button onClick={()=>handledelete(list.id)}>delete</button>
+                    <input type="checkbox"/>
+                    <span className="items">{list.task}</span>
+                    <button className="update" onClick={() => handleupdate(i)}>update</button>
+                    <button className="delete" onClick={() => handledelete(list.id)}>delete</button>
                 </div>
             ))}
         </div>

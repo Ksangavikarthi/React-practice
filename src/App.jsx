782fc008f2ab1169ceb0ecfcc1f7c599e27cfc25
Ksangavikarthi <ksangavi2002@gmail.com>
2024-01-12 20:23:31 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { Suspense, lazy, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -22,7 +22,26 @@ import YupValid from './Components/Functioncomp/YupValid'
 import TodoListfun from './Components/Functioncomp/TodoListfun'
 import Axiostodolistfun from './Components/Functioncomp/Axiostodolistfun'
 import Propsdrilling from './Components/Functioncomp/Propsdrilling'
+import CustomUSeFetch from './Components/Functioncomp/CustomUseFetch'
+import CustomUSeFetch1 from './Components/Functioncomp/CustomUseFetch1'
+import ErrorBoundary from './Components/ClassComp/ErrorBoundary'
+import ReactRouter from './Components/Functioncomp/ReactRouter'
+import ReactRouter1 from './Components/Functioncomp/ReactRouter1'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import DynamicImport from './Components/Functioncomp/DynamicImport'
+import { withfetchapi } from './Components/Functioncomp/Hoc/withfetchapi'
+import Datahoc from './Components/Functioncomp/Hoc/Datahoc'
+import CounterProvider from './Components/ContextApi/CounterProvider/CounterProvider'
+import HomeProvide from './Components/Functioncomp/HomeProvide'
+import ProductProvide from './Components/Functioncomp/ProductProvide'
 
+
+const Datalist=withfetchapi(Datahoc,'https://jsonplaceholder.typicode.com/todos/1')
+
+//-----------------Lazy loading--------
+
+// const ReactRouter=lazy(()=>import('./Components/Functioncomp/ReactRouter'))
+// const ReactRouter1=lazy(()=>import('./Components/Functioncomp/ReactRouter1'))
 
 function App() {
 //   const [tr, settr] = useState(true)
@@ -33,6 +52,8 @@ function App() {
 
  return (
     <>
+
+<ErrorBoundary>
     {/* <Welcome/> */}
     {/* <Intro title={'kavi'} /> */}
     {/* <Diffmethodprops  movie={'irugapatru'}/> */}
@@ -52,11 +73,43 @@ function App() {
     {/* <Reactform/> */}
     {/* <YupValid/> */}
     {/* <Reacthookform/> */}
-    {/* <TodoListfun/> */}
+    <TodoListfun/>
     {/* <Axiostodolistfun/> */}
     {/* {tr && <Lifecyclefun/>} 
     <button onClick={test}>click</button> */}
-    <Propsdrilling/>
+ 
+       {/* <CustomUSeFetch />
+        <CustomUSeFetch1 /> */}
+        {/* <DynamicImport/> */}
+{/* <Reducercomp/> */}
+{/* <Datalist/> */}
+
+{/* <CounterProvider>
+    <HomeProvide/>
+    <ProductProvide/>
+</CounterProvider> */}
+
+
+
+      
+
+{/* <Suspense fallback={ <h1>loading......</h1>  }>
+<BrowserRouter>
+<ul>
+  <li> <Link to='/ReactRouter/4'>ReactRouter</Link> </li>
+  <li>  <Link to='/ReactRouter1'>ReactRouter1</Link></li>
+</ul>
+
+<Routes>
+  <Route path='/ReactRouter/:id' element={<ReactRouter/>}/>
+  <Route path='/ReactRouter1' element={<ReactRouter1/>}/>
+</Routes>
+</BrowserRouter>
+</Suspense> */}
+      
+</ErrorBoundary>
+
+    {/* <Propsdrilling/> */}
 
 
 
